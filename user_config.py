efefa6a71aba31691ef293b24162d136bf3a41d8
lmh0757a <lmh490023820@gmail.com>
2024-08-12 18:08:27 +0800

    Create user_config.py

---
+++ user_config.py
@@ -0,0 +1,42 @@
+open_update = True
+open_use_old_result = True
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+    "广东珠江",
+    "CCTV-1",
+    "CCTV-6",
+    "CCTV-8",
+    "CCTV-13",
+    "广州新闻",
+    "广州综合",
+    "广州影视",
+    "大湾区卫视",
+    "浙江卫视",
+    "湖南卫视",
+    "东方卫视",
+]
+open_online_search = False
+favorite_page_num = 5
+default_page_num = 3
+urls_limit = 10
+open_keep_all = False
+open_sort = True
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv4"
+domain_blacklist = ["epg.pw"]
+url_keywords_blacklist = []
+open_subscribe = False
+subscribe_urls = [
+    "https://m3u.ibert.me/txt/fmml_dv6.txt",
+    "https://m3u.ibert.me/txt/o_cn.txt",
+    "https://m3u.ibert.me/txt/j_iptv.txt",
+    "https://github.moeyy.xyz/https://raw.githubusercontent.com/PizazzGY/TVBox/main/live.txt",
+]
+open_multicast = True
+region_list = ["广东"]
+open_proxy = True
+open_driver = False
+open_use_old_result = True
